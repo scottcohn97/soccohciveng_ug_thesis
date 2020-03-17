d2e3026b49b405544ea6faccb267788e1db014d2
scottkc97 <scott97kc@gmail.com>
2020-03-17 12:09:34 -0400

    belonging Q

--- Code/data_sim_full.R
+++ Code/data_sim_full.R
@@ -42,10 +42,10 @@ T1_34 <- .matchL(get(ID$strategy[3]), get(ID$strategy[4]), 10)
 # match 1 payoffs
 # Task1_Player_Match
 
-T1_1_1 <- T1_12$P[,1]
-T1_2_1 <- T1_12$P[,2]
-T1_3_1 <- T1_34$P[,1]
-T1_4_1 <- T1_34$P[,2]
+T1_1_1 <- T1_12$P[,1] %>% append(1)
+T1_2_1 <- T1_12$P[,2] %>% append(2)
+T1_3_1 <- T1_34$P[,1] %>% append(3)
+T1_4_1 <- T1_34$P[,2] %>% append(4)
 
 # match 2
 T1_13 <- .matchL(get(ID$strategy[1]), get(ID$strategy[3]), 10)
@@ -53,10 +53,10 @@ T1_24 <- .matchL(get(ID$strategy[2]), get(ID$strategy[4]), 10)
 
 # match 2 payoffs
 
-T1_1_2 <- T1_13$P[,1]
-T1_2_2 <- T1_24$P[,1]
-T1_3_2 <- T1_13$P[,2]
-T1_4_2 <- T1_24$P[,2]
+T1_1_2 <- T1_13$P[,1] %>% append(1)
+T1_2_2 <- T1_24$P[,1] %>% append(2)
+T1_3_2 <- T1_13$P[,2] %>% append(3)
+T1_4_2 <- T1_24$P[,2] %>% append(4)
 
 # match 3
 T1_14 <- .matchL(get(ID$strategy[1]), get(ID$strategy[4]), 10)
@@ -64,10 +64,10 @@ T1_23 <- .matchL(get(ID$strategy[2]), get(ID$strategy[3]), 10)
 
 # match 3 payoffs
 
-T1_1_3 <- T1_14$P[,1]
-T1_2_3 <- T1_23$P[,1]
-T1_3_3 <- T1_23$P[,2]
-T1_4_3 <- T1_14$P[,2]
+T1_1_3 <- T1_14$P[,1] %>% append(1)
+T1_2_3 <- T1_23$P[,1] %>% append(2)
+T1_3_3 <- T1_23$P[,2] %>% append(3)
+T1_4_3 <- T1_14$P[,2] %>% append(4)
 
 # High Tx - B
 # match 1
@@ -76,10 +76,10 @@ T1_78 <- .matchH(get(ID$strategy[7]), get(ID$strategy[8]), 10)
 
 # match 1 payoffs
 
-T1_5_1 <- T1_56$P[,1]
-T1_6_1 <- T1_56$P[,2]
-T1_7_1 <- T1_78$P[,1]
-T1_8_1 <- T1_78$P[,2]
+T1_5_1 <- T1_56$P[,1] %>% append(5)
+T1_6_1 <- T1_56$P[,2] %>% append(6)
+T1_7_1 <- T1_78$P[,1] %>% append(7)
+T1_8_1 <- T1_78$P[,2] %>% append(8)
 
 # match 2
 T1_57 <- .matchH(get(ID$strategy[5]), get(ID$strategy[7]), 10)
@@ -87,43 +87,52 @@ T1_68 <- .matchH(get(ID$strategy[6]), get(ID$strategy[8]), 10)
 
 # match 2 payoffs
 
-T1_5_2 <- T1_57$P[,1]
-T1_6_2 <- T1_68$P[,1]
-T1_7_2 <- T1_57$P[,2]
-T1_8_2 <- T1_68$P[,2]
+T1_5_2 <- T1_57$P[,1] %>% append(5)
+T1_6_2 <- T1_68$P[,1] %>% append(6)
+T1_7_2 <- T1_57$P[,2] %>% append(7)
+T1_8_2 <- T1_68$P[,2] %>% append(8)
 
 # match 3
 T1_58 <- .matchH(get(ID$strategy[5]), get(ID$strategy[8]), 10)
-T1_67 <- .matchH(get(ID$strategy[6]), get(ID$strategy[7]), 10)
+T1_67 <- .matchH(get(ID$strategy[6]), get(ID$strategy[7]), 10) 
 
 # match 3 payoffs
 
-T1_5_3 <- T1_58$P[,1]
-T1_6_3 <- T1_67$P[,1]
-T1_7_3 <- T1_67$P[,2]
-T1_8_3 <- T1_58$P[,2]
+T1_5_3 <- T1_58$P[,1] %>% append(5)
+T1_6_3 <- T1_67$P[,1] %>% append(6)
+T1_7_3 <- T1_67$P[,2] %>% append(7)
+T1_8_3 <- T1_58$P[,2] %>% append(8)
 
 # Payoff Table ---------------------------------------------------------
 
-columns = sprintf("c%d", 1:10)
-idrow <- c("ID", 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4)
+columns <- sprintf("c%d", 1:10) %>% append("partID")
+
 # join tables
 payoffsL <- tibble(columns, T1_1_1, T1_2_1, T1_3_1, T1_4_1,
                   T1_1_2, T1_2_2, T1_3_2, T1_4_2,
                   T1_1_3, T1_2_3, T1_3_3, T1_4_3) 
-payoffsL <- rbind(payoffsL, idrow)
-# TODO figure out ID and reformatting this
-  
 
 payoffsH <- tibble(columns, T1_5_1, T1_6_1, T1_7_1, T1_8_1,
                    T1_5_2, T1_6_2, T1_7_2, T1_8_2,
                    T1_5_3, T1_6_3, T1_7_3, T1_8_3)
 
-# Belonging Question ------------------------------------------------------
+# Belonging Questions ------------------------------------------------------
 
 likert4 <- 1:4
 likert5 <- 1:5
 
+belQ <- function(){
+  Q1 <- sample(likert4, 1, replace = TRUE)
+  Q2_3 <- sample(likert5, 2, replace = TRUE)
+  return(append(Q1, Q2_3))
+}
+
+x <- c("partID", "Q1", "Q2", "Q3")
+belongingQuestions <- as.data.frame(sapply(x, function(x) numeric()))
+
+for (i in 1:8) {
+  belongingQuestions[i, ] <- append(i, belQ())
+}
 
 # TPGG --------------------------------------------------------------------
 
